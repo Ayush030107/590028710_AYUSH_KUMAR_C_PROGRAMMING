4839aec8614b0171f7741c6abc4719672d64d75f
AYUSH KUMAR <ayush070925@gmail.com>
2025-10-31 19:39:24 +0530

    Update and rename experiment_8.c to 08_experiment_8.c

--- 08_experiment_8.c
+++ 08_experiment_8.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main()
-{
-    int x = 5;
+int main() {
+    int x;
+
+    printf("Enter an integer: ");
+    scanf("%d", &x);
 
     printf("Initial value of x: %d\n", x);
-
     printf("Value using post-increment (x++): %d\n", x++);
     printf("Value of x after post-increment: %d\n", x);
-
     printf("Value using pre-increment (++x): %d\n", ++x);
     printf("Value of x after pre-increment: %d\n", x);
 
     return 0;
 }
+
