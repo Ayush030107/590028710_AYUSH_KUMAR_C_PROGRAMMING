d4a6b7c56886c75944a5d93e722847af515935e5
AYUSH KUMAR <ayush070925@gmail.com>
2025-10-31 19:37:25 +0530

    Update and rename experiment_7.c to 07_experiment_7.c

---
+++ 07_experiment_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    (num & 1) ? printf("Odd\n") : printf("Even\n");
+
+    return 0;
+}
